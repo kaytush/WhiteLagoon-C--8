28def2658f5f35726835216947380f48f3635803
Abhishek Sahai <abhishek.sahai@outlook.in>
2023-10-04 23:49:49 +0530

    Add Villa Controller

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas;
+            return View(villas);
+        }
+    }
+}
